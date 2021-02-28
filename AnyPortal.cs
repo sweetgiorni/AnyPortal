6dac2636bf714a0b6e8d9d82863874f86577aa7e
Alex Sweet <sweet.giorni@gmail.com>
2021-02-28 07:57:44 -0800

    Update to 1.0.3

--- AnyPortal.cs
+++ AnyPortal.cs
@@ -12,7 +12,7 @@ using UnityEngine.UI;
 
 namespace AnyPortal
 {
-    [BepInPlugin("org.sweetgiorni.plugins.anyportal", "AnyPortal", "1.0.2")]
+    [BepInPlugin("org.sweetgiorni.plugins.anyportal", "AnyPortal", "1.0.3")]
     public class AnyPortal : BaseUnityPlugin
     {
         public Harmony harmony;
