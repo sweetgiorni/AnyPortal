fba162c5afbf6fb73ef236f8f05948a48600408e
Alex Sweet <sweet.giorni@gmail.com>
2021-02-27 21:39:28 -0800

    Create release script

---
+++ create_release.sh
@@ -0,0 +1,23 @@
+# REMEMBER TO UPDATE MOD VERSION IN AssemblyInfo.cs, AnyPortal.cs, AND manifest.json!!!
+# Create a release zip suitable for Nexus and Thunderstore
+
+set -x
+set -e
+
+RELEASE_DIR="AnyPortalRelease"
+
+rm -rf $RELEASE_DIR
+
+mkdir -p $RELEASE_DIR/AnyPortal
+
+# Game lib and AssetBundle
+cp bin/Debug/AnyPortal.dll $RELEASE_DIR/AnyPortal
+cp anyportal $RELEASE_DIR/AnyPortal
+
+# Other stuff...
+cp icon.png $RELEASE_DIR
+cp manifest.json $RELEASE_DIR
+cp README.md $RELEASE_DIR
+
+rm -f AnyPortal.zip
+cd $RELEASE_DIR; zip -r ../AnyPortal.zip *
